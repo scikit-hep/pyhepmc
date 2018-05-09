3fa8ac5bbfdb3449896f8795a0e9ff5ed3fe8b9d
Hans Dembinski <hans.dembinski@gmail.com>
2018-05-09 15:15:48 +0200

    more brief setup.py

--- setup.py
+++ setup.py
@@ -11,13 +11,7 @@ __version__ = '0.0.1'
 hepmc_source = glob.glob('src/HepMC3/src/*.cc') + glob.glob('src/HepMC3/src/Search/*.cc')
 
 
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
-
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
-
+class lazy_get_pybind_include:
     def __init__(self, user=False):
         self.user = user
 
@@ -31,23 +25,16 @@ ext_modules = [
         'pyhepmc',
         ['src/main.cpp'] + hepmc_source,
         include_dirs=[
-            # Path to pybind11 headers
-            get_pybind_include(),
-            get_pybind_include(user=True),
-            # Path to HepMC3 headers
             'src/HepMC3/include',
+            lazy_get_pybind_include(user=True),
+            lazy_get_pybind_include(),
         ],
         language='c++'
     ),
 ]
 
 
-# As of Python 3.6, CCompiler has a `has_flag` method.
-# cf http://bugs.python.org/issue26689
 def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
     import tempfile
     with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
         f.write('int main (int argc, char **argv) { return 0; }')
@@ -59,32 +46,26 @@ def has_flag(compiler, flagname):
 
 
 def cpp_flag(compiler):
-    """Return the -std=c++[11/14] compiler flag.
-
-    The c++14 is prefered over c++11 (when it is available).
-    """
     if has_flag(compiler, '-std=c++14'):
         return '-std=c++14'
     elif has_flag(compiler, '-std=c++11'):
         return '-std=c++11'
     else:
-        raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                           'is needed!')
+        raise RuntimeError('Compiler does not support C++11')
 
 
 class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-    c_opts = {
+    compile_flags = {
         'msvc': ['/EHsc'],
         'unix': [],
     }
 
     if sys.platform == 'darwin':
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+        compile_flags['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
 
     def build_extensions(self):
         ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
+        opts = self.compile_flags.get(ct, [])
         if ct == 'unix':
             opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
             opts.append(cpp_flag(self.compiler))
@@ -95,6 +76,7 @@ class BuildExt(build_ext):
         for ext in self.extensions:
             ext.extra_compile_args = opts
         build_ext.build_extensions(self)
+
 
 setup(
     name='pyhepmc',
