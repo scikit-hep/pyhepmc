5029122bc2cea3563c43471375290be27e3ce55f
Hans Dembinski <hans.dembinski@gmail.com>
2019-08-13 23:52:15 +0200

    much simpler setup.py

--- setup.py
+++ setup.py
@@ -3,91 +3,47 @@ License: pyhepmc-ng is covered by the BSD license, but the license only
 applies to the binding code. The HepMC3 code is covered by the GPL-v3 license.
 """
 from setuptools import setup, find_packages, Extension
-from distutils.command.build_ext import build_ext
-from setuptools import distutils
 import sys
 import os
 import glob
+import subprocess as subp
+import tempfile
 
 
-def lazy_compile(self, sources, output_dir=None, macros=None,
-                 include_dirs=None, debug=0, extra_preargs=None,
-                 extra_postargs=None, depends=None):
-    macros, objects, extra_postargs, pp_opts, build = \
-            self._setup_compile(output_dir, macros, include_dirs, sources,
-                                depends, extra_postargs)
-    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
+compile_flags = {
+    'unix': (
+        '-fvisibility=hidden',
+        '-stdlib=libc++',
+        # ignore warnings raised by HepMC3 code
+        '-Wno-deprecated-register',
+        '-Wno-strict-aliasing',
+        '-Wno-sign-compare',
+        '-Wno-reorder'
+    )
+}
 
-    for obj in objects:
-        try:
-            src, ext = build[obj]
-        except KeyError:
-            continue
-        if not os.path.exists(obj) or os.stat(obj).st_mtime < os.stat(src).st_mtime:
-            self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
-    return objects
 
+# override class method to inject platform-specific compiler flags
+def patched_compile(self, sources, **kwargs):
+    if not hasattr(self, "my_extra_flags"):
+        self.my_extra_flags = []
+        cmd = self.compiler[0]
+        with open(os.devnull, 'w') as devnull:
+            with tempfile.TemporaryDirectory() as tmpdir:
+                with open(tmpdir + "/main.cpp", "w") as f:
+                    f.write('int main() {}')
+                for flag in compile_flags.get(self.compiler_type, []):
+                    retcode = subp.call((cmd, flag, "main.cpp"),
+                                        cwd=tmpdir,
+                                        stderr=devnull)
+                    if retcode == 0:
+                        self.my_extra_flags.append(flag)
+    kwargs['extra_preargs'] = kwargs.get('extra_preargs', []) + self.my_extra_flags
+    return self.original_compile(sources, **kwargs)
 
 import distutils.ccompiler
-distutils.ccompiler.CCompiler.compile = lazy_compile
-
-
-hepmc_source = glob.glob('extern/HepMC3/src/*.cc') + glob.glob('extern/HepMC3/src/Search/*.cc')
-hepmc_include = 'extern/HepMC3/include'
-pybind11_include = 'extern/pybind11/include'
-ext_modules = [
-    Extension(
-        'pyhepmc_ng._bindings',
-        ['src/bindings.cpp'] + hepmc_source,
-        include_dirs=[
-            hepmc_include,
-            pybind11_include,
-            'src',
-        ],
-        language='c++'
-    ),
-]
-
-
-def flag_filter(compiler, *flags):
-    import subprocess as subp
-    import tempfile
-    import os
-    cmd = compiler.compiler[0]
-    accepted = []
-    devnull = open(os.devnull, 'w')
-    with tempfile.TemporaryDirectory() as tmpdir:
-        with open(tmpdir + "/main.cpp", "w") as f:
-            f.write('int main() {}')
-        for flag in flags:
-            retcode = subp.call((cmd, flag, "main.cpp"),
-                                cwd=tmpdir,
-                                stderr=devnull)
-            if retcode == 0:
-                accepted.append(flag)
-    return accepted
-
-
-class BuildExt(build_ext):
-    # these flags are not checked and always added
-    compile_flags = {"msvc": ['/EHsc'], "unix": ["-std=c++11"]}
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.compile_flags.get(ct, [])
-        if ct == 'unix':
-            # only add flags which pass the flag_filter
-            opts += flag_filter(self.compiler,
-                                '-fvisibility=hidden',
-                                '-stdlib=libc++',
-                                # ignore warnings raised by HepMC3 code
-                                '-Wno-deprecated-register',
-                                '-Wno-strict-aliasing',
-                                '-Wno-sign-compare',
-                                '-Wno-reorder')
-        for ext in self.extensions:
-            ext.extra_compile_args = opts
-        build_ext.build_extensions(self)
+distutils.ccompiler.CCompiler.original_compile = distutils.ccompiler.CCompiler.compile
+distutils.ccompiler.CCompiler.compile = patched_compile
 
 
 def get_version():
@@ -129,7 +85,16 @@ setup(
     extras_require={
         "tests": ['pytest', 'numpy', 'graphviz', 'particle'],
     },
-    ext_modules=ext_modules,
-    cmdclass={'build_ext': BuildExt},
+    ext_modules=[
+        Extension('pyhepmc_ng._bindings',
+            ['src/bindings.cpp'] + glob.glob('extern/HepMC3/src/*.cc')
+            + glob.glob('extern/HepMC3/src/Search/*.cc'),
+            include_dirs=[
+                'extern/HepMC3/include',
+                'extern/pybind11/include',
+                'src',
+            ],
+            language='c++')
+    ],
     zip_safe=False,
 )
