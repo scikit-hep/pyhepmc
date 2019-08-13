b717e0952efcfac6cc07164edc996f1912d10125
Hans Dembinski <hans.dembinski@gmail.com>
2019-08-13 23:53:03 +0200

    more python versions

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='generator montecarlo simulation data hep physics particle',
     packages=find_packages('src'),
