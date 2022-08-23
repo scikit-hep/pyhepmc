604ce867a40d7dd3704320b28972a5528db758e3
Hans Dembinski <hans.dembinski@gmail.com>
2022-08-23 13:11:52 +0200

    handle invalid particles and events without history

--- src/pyhepmc/view.py
+++ src/pyhepmc/view.py
@@ -1,7 +1,8 @@
 from graphviz import Digraph
-from particle import Particle, ParticleNotFound
+from particle import Particle, ParticleNotFound, InvalidParticle
 from pyhepmc._prettify import db as _prettify
 from pyhepmc import Units
+import numpy as np
 
 
 def to_dot(evt, style=None):
@@ -17,19 +18,28 @@ def to_dot(evt, style=None):
     for v in evt.vertices:
         d.node(f"{v.id}", height="0.02", color=gray, tooltip=f"status={v.status}")
 
-    vi = 0
-    vo = 0
     for p in evt.particles:
         try:
             pdb = Particle.from_pdgid(p.pid)
             charge = pdb.charge
             pname = pdb.name
             pname = _prettify.get(pdb.pdgid, pname)
-            tooltip = str(pdb)
+            tooltip = pdb.name
+            if pdb.quarks:
+                tooltip += f" ({pdb.quarks})"
+            tooltip += f"\nQ = {pdb.charge:.3g}"
+            if pdb.mass:
+                tooltip += f"\nm = {pdb.mass:.4g} MeV/c2"
+            if pdb.ctau and np.isfinite(pdb.ctau):
+                tooltip += f"\ncτ = {pdb.ctau:.3g} mm"
         except ParticleNotFound:
             pname = f"Internal({p.pid})"
             charge = 0
-            tooltip = ""
+            tooltip = pname
+        except InvalidParticle:
+            pname = f"Invalid({p.pid})"
+            charge = 0
+            tooltip = pname
         en = p.momentum.e / GeV
         unit = "GeV"
         if en > 1e2:
@@ -39,44 +49,44 @@ def to_dot(evt, style=None):
             en *= 1e3
             unit = "MeV"
 
-        label = f" {pname} {en:.2g} {unit}"
-        style = "solid" if charge else "dashed"
-        if not p.parents:  # initial particles
-            vid = f"in_{vi}"
-            vi += 1
-            d.node(vid, style="invis")
-            assert p.end_vertex
-            d.edge(
-                vid,
-                f"{p.end_vertex.id}",
-                label=label,
-                arrowsize="1",
-                style="bold",
-                tooltip=tooltip,
-            )
-        elif not p.children:  # final state particles
-            vid = f"out_{vo}"
-            vo += 1
-            d.node(vid, style="invis")
-            assert p.production_vertex
-            d.edge(
-                f"{p.production_vertex.id}",
-                vid,
-                label=label,
-                style=style,
-                tooltip=tooltip,
-            )
+        label = f"{pname} {en:.2g} {unit}"
+
+        # do not connect initial particles to root vertex
+        if p.production_vertex and p.production_vertex.id != 0:
+            vin = f"{p.production_vertex.id}"
         else:
-            # intermediate particles
-            d.edge(
-                f"{p.production_vertex.id}",
-                f"{p.end_vertex.id}",
-                label=label,
-                color=gray,
-                fontcolor=gray,
-                style=style,
-                dir="none",
-                tooltip=tooltip,
-            )
+            vin = f"in_{p.id}"
+            d.node(vin, style="invis")
+
+        if p.end_vertex:
+            vout = f"{p.end_vertex.id}"
+        else:
+            vout = f"out_{p.id}"
+            d.node(vout, style="invis")
+
+        style = "solid" if charge else "dashed"
+        if not p.production_vertex or p.production_vertex.id == 0 and p.end_vertex:
+            # initial particle which is not also final state is beam particle
+            style = "bold"
+            edir = "forward"
+            color = None
+        elif p.status == 1:  # final state
+            edir = "forward"
+            color = None
+        else:  # intermediate
+            edir = "none"
+            color = gray
+
+        d.edge(
+            vin,
+            vout,
+            label=label,
+            dir=edir,
+            style=style,
+            color=color,
+            fontcolor=color,
+            tooltip=tooltip,
+            labeltooltip=tooltip,
+        )
 
     return d
