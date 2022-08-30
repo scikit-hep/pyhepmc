1e28b7ebdec988a0b96fac455c33fc3ae3c185dd
Hans Dembinski <HDembinski@users.noreply.github.com>
2022-08-30 17:40:23 +0200

    improved version sourcing (#32)
    Using `:attr:` to get the version from pyhepmc/_version.py does not work when the installation fails due to some compilation error, making pyhepmc unimportable. `:attr:` is supposed to not invoke the Python interpreter to get the version literal, but for some reason this is not working. I suppose this only works when the version is a literal in `__init__.py`, but it is a literal in `pyhepmc/_version.py`. Since we cannot get rid of `setup.py`, because we need this to declare the CMakeExtensions there, we can just as well safely pull the version by `exec`ing `_version.py` from there.

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from pathlib import Path
 from setuptools import setup
 import sys
 
-cwd = Path(__file__).parent
+cdir = Path(__file__).parent.absolute()
 
-sys.path.append(str(cwd))
+sys.path.append(str(cdir))
 from cmake_ext import CMakeExtension, CMakeBuild  # noqa: E402
 
 setup(
     zip_safe=False,
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     ext_modules=[CMakeExtension("pyhepmc._core")],
     cmdclass={"build_ext": CMakeBuild},
 )
