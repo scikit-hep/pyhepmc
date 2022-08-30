786dc2d37f5cacdc6d2b18a4d4db2b535957a7d6
Hans Dembinski <hans.dembinski@gmail.com>
2022-08-30 16:59:03 +0200

    improved version sourcing

--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@ from pathlib import Path
 from setuptools import setup
 import sys
 
-cwd = Path(__file__).parent
+cdir = Path(__file__).parent
 
-sys.path.append(str(cwd))
+version = {}
+with open(cdir / "src" / "pyhepmc" / "_version.py") as f:
+    exec(f.read(), version)
+
+sys.path.append(str(cdir))
 from cmake_ext import CMakeExtension, CMakeBuild  # noqa: E402
 
 setup(
     zip_safe=False,
+    version=version["version"],
     ext_modules=[CMakeExtension("pyhepmc._core")],
     cmdclass={"build_ext": CMakeBuild},
 )
