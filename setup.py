91a8b40e7f3562cd08d3632c33b0ca2c195eb479
Hans Dembinski <hans.dembinski@gmail.com>
2022-08-30 17:12:37 +0200

    fix

--- setup.py
+++ setup.py
@@ -4,16 +4,13 @@ import sys
 
 cdir = Path(__file__).parent.absolute()
 
-version = {}
-with open(cdir / "src" / "pyhepmc" / "_version.py") as f:
-    exec(f.read(), version)
-
 sys.path.append(str(cdir))
 from cmake_ext import CMakeExtension, CMakeBuild  # noqa: E402
 
 setup(
     zip_safe=False,
-    version=version["version"],
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     ext_modules=[CMakeExtension("pyhepmc._core")],
     cmdclass={"build_ext": CMakeBuild},
 )
