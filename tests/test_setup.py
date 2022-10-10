cf70d896bf5ebd7bdeacff54f51ca0d2aa0e76f7
Hans Dembinski <hans.dembinski@gmail.com>
2022-10-10 11:27:55 +0200

    finish

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -2,8 +2,6 @@ import pyhepmc
 
 
 def test_all():
-    print(pyhepmc.__version__)
-
     assert pyhepmc.print_errors()
     assert pyhepmc.print_warnings()
 
@@ -14,3 +12,7 @@ def test_all():
     pyhepmc.set_print_warnings(False)
     assert not pyhepmc.print_errors()
     assert not pyhepmc.print_warnings()
+
+    assert pyhepmc.debug_level() == 5
+    pyhepmc.set_debug_level(10)
+    assert pyhepmc.debug_level() == 10
