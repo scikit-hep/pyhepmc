3c358f79da1ea97dd2a7d7b55bbc777d97ebb6b6
Hans Dembinski <hans.dembinski@gmail.com>
2023-01-21 09:52:00 +0100

    fix

--- src/geneventdata.cpp
+++ src/geneventdata.cpp
@@ -2,7 +2,6 @@
 #include <HepMC3/Data/GenEventData.h>
 #include <HepMC3/Data/GenParticleData.h>
 #include <HepMC3/Data/GenVertexData.h>
-#include <pybind11/numpy.h>
 
 using namespace HepMC3;
 
@@ -24,12 +23,12 @@ void register_geneventdata_dtypes() {
   PYBIND11_NUMPY_DTYPE(VertexData, status, x, y, z, t);
 }
 
-#define MAKE(name, type)                                      \
-  py::object GenEventData_##name(py::object self) {           \
-    auto& s = py::cast<GenEventData&>(self);                  \
-    ssize_t shape[1] = {static_cast<ssize_t>(s.name.size())}; \
-    auto dt = py::dtype::of<type>();                          \
-    return py::array(dt, shape, s.name.data(), self);         \
+#define MAKE(name, type)                                              \
+  py::object GenEventData_##name(py::object self) {                   \
+    auto& s = py::cast<GenEventData&>(self);                          \
+    py::ssize_t shape[1] = {static_cast<py::ssize_t>(s.name.size())}; \
+    auto dt = py::dtype::of<type>();                                  \
+    return py::array(dt, shape, s.name.data(), self);                 \
   }
 
 MAKE(weights, double)
